27771ef5d0f12cead5caffb9497254597630b325
Juho <juho.ahonen2@gmail.com>
2022-04-26 16:32:10 +0300

    added constants

---
+++ src/modules/constants.php
@@ -0,0 +1,5 @@
+<?php
+    define("BASE_DIR", realpath(__DIR__."/../../")."/");
+    define("PUBLIC_DIR", BASE_DIR."public/");
+    define("MODULES_DIR", BASE_DIR."src/modules/");
+    define("TEMPLATES_DIR", BASE_DIR."src/templates/");
