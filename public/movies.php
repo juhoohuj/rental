2277b8fc1253fbfdae9f3669b26c5ffea8771855
Jussi Kujala <jussijustus@gmail.com>
2022-04-27 16:00:16 +0300

    movies.php moduuli yhdistetty movie.php moduuliin ja delete nappi korjattu elokuvat sivulla

--- public/movies.php
+++ public/movies.php
@@ -1,10 +1,22 @@
 <?php
 include TEMPLATES_DIR."head.php";
-include MODULES_DIR."movies.php";
+include MODULES_DIR."movie.php";
 
-// Get all people from database
+$id = filter_input(INPUT_GET, "id");
+//if id exists delete
+if(isset($id)){
+    try{
+        deleteMovie($id);
+        echo '<div class="alert alert-success" role="alert">Movie deleted!!</div>';
+    }catch(Exception $e){
+        echo '<div class="alert alert-danger" role="alert">'.$e->getMessage().'</div>';
+    }
+    
+}
+
+// Get all movies from database
 $movie = getMovies();
-// Print person list
+// Print movies in a list
 echo "<ul>";
 foreach($movie as $m){
     echo "<li>".$m["Name"]." ".$m["Length"]." ".$m["Language"]." ".$m["Genre"]. '<a href="movies.php?id=' . $m["MovieID"] . '" class="btn btn-primary">Delete</a> </li>';
