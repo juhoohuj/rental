caf2586a1d71d6b6581c954c056c007a7d21153c
Jussi Kujala <jussijustus@gmail.com>
2022-04-27 19:46:52 +0300

    luontilauseet päivitetty ja moviecopy taulu poistettu

--- sql_scripts/db.sql
+++ sql_scripts/db.sql
@@ -4,6 +4,7 @@ CREATE TABLE MOVIE (
     Length time,
     Language varchar(255),
     Genre varchar(255),
+    Status varchar(255),
     PRIMARY KEY (MovieID)
 );
 
@@ -13,22 +14,13 @@ CREATE TABLE CUSTOMER (
     SurName varchar(255)
 );
 
-CREATE TABLE MOVIECOPY (
-  CopyID INT AUTO_INCREMENT,
-  MovieID INT,
-  Status char(1),
-  PRIMARY KEY(COPYID),
-  FOREIGN KEY (MovieID) REFERENCES MOVIE(MovieID)
-  );
 
 CREATE TABLE RENT (
     RentID INT AUTO_INCREMENT PRIMARY KEY,
     CustomerID INT,
     MovieID INT,
-    CopyID INT,
     startDate DATETIME,
     endDate DATETIME,
     FOREIGN KEY (CustomerID) REFERENCES CUSTOMER(CustomerID),
-    FOREIGN KEY (MovieID) REFERENCES MOVIE(MovieID),
-    FOREIGN KEY (CopyID) REFERENCES MOVIECOPY(CopyID)
+    FOREIGN KEY (MovieID) REFERENCES MOVIE(MovieID)
 );
