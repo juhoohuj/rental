ccaaf44132060d8d48bf8ee9a6cc6677857a2fad
Jussi Kujala <jussijustus@gmail.com>
2022-04-27 15:27:19 +0300

    muutoksia movies.php tiedostoihin ja delete nappi lisätty

--- public/movies.php
+++ public/movies.php
@@ -1,26 +1,14 @@
 <?php
-include TEMPLATES_DIR.'head.php';
-include MODULES_DIR. 'movies.php';
+include TEMPLATES_DIR."head.php";
+include MODULES_DIR."movies.php";
 
-//elokuvien haku tietokannasta
-$movies = getMovies();
-?>
-
-<h2>Elokuvat</h2>
-<table class="table table-striped">
-    <tr>
-        <th>Nimi</th>
-        <th>Kesto</th>
-        <th>Kieli</th>
-        <th>Genre</th>
-    </tr>
-
-    <?php
-    //elokuvat tulostetaan tauluun
-foreach($movies as $movielist){
-    echo "<tr><td>".$movielist["Name"]."</td><td>" . $movielist["Length"]."</td><td>" . $movielist["Language"]."</td><td>".$movielist["Genre"].'</td><td>'  . '</tr>';
+// Get all people from database
+$movie = getMovies();
+// Print person list
+echo "<ul>";
+foreach($movie as $m){
+    echo "<li>".$m["Name"]." ".$m["Length"]." ".$m["Language"]." ".$m["Genre"]. '<a href="movies.php?id=' . $m["MovieID"] . '" class="btn btn-primary">Delete</a> </li>';
 }
-?>
-</table>
-<?php
-include TEMPLATES_DIR.'foot.php'; ?>
+echo "</ul>";
+
+include TEMPLATES_DIR.'foot.php';
--- src/modules/movies.php
+++ src/modules/movies.php
@@ -6,7 +6,7 @@ function getMovies(){
     try{
         $pdo = openDB();
         // Create SQL query to get all rows from a table
-        $sql = "SELECT Name, Length, Language, Genre
+        $sql = "SELECT Name, Length, Language, Genre, MovieID
         FROM movie
         ORDER BY name;";
         // Execute the query
@@ -14,6 +14,31 @@ function getMovies(){
 
         return $movies->fetchAll();
     }catch(PDOException $e){
+        throw $e;
+    }
+}
+
+function deleteMovie($id){
+    require_once MODULES_DIR.'db.php'; // DB connection
+    
+    //Tarkistetaan onko muttujia asetettu
+    if( !isset($id) ){
+        throw new Exception("Missing parameters! Cannot delete person!");
+    }
+    
+    try{
+        $pdo = openDB();
+        // Start transaction
+        $pdo->beginTransaction();
+        // Delete from worktime table
+        $sql = "DELETE FROM movie WHERE MovieID = ?";
+        $statement = $pdo->prepare($sql);
+        $statement->bindParam(1, $id);        
+        $statement->execute();
+        $pdo->commit();
+    }catch(PDOException $e){
+        // Rollback transaction on error
+        $pdo->rollBack();
         throw $e;
     }
 }
