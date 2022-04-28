252d3ead6a67e54ca117c5033fec8d6d457e7636
Siiri Poropudas <n1posi00@students.oamk.fi>
2022-04-28 23:35:26 +0300

    [Work in progress] Customer & movie names display

--- public/rentHistory.php
+++ public/rentHistory.php
@@ -4,9 +4,19 @@ include MODULES_DIR."rentals.php";
 
 
 $rental = getRents();
+$movieName = getMovieName();
+$customerName = getCustomerName();
 
 foreach($rental as $r){
-    echo "<li>".$r["startDate"]." - ".$r["endDate"]."  ".$r["CustomerID"]."  ".$r["MovieID"]."<br>";
+    echo "<li>".$r["startDate"]." - ".$r["endDate"];
+}
+
+foreach($movieName as $mn){
+    echo $mn["Name"];
+}
+
+foreach($customerName as $cn){
+    echo $cn["FirstName"]." ".$cn["SurName"];
 }
 
 include TEMPLATES_DIR.'foot.php';
--- src/modules/rentals.php
+++ src/modules/rentals.php
@@ -14,3 +14,33 @@ function getRents(){
         throw $error;
     }
 }
+
+function getMovieName(){
+    require_once MODULES_DIR.'db.php';
+
+    try{
+        $pdo = openDB();
+        $sql = "SELECT Name
+        FROM movie";
+        $rental = $pdo->query($sql);
+
+        return $rental->fetchAll();
+    }catch(PDOException $error){
+        throw $error;
+    }
+}
+
+function getCustomerName(){
+    require_once MODULES_DIR.'db.php';
+
+    try{
+        $pdo = openDB();
+        $sql = "SELECT FirstName, SurName
+        FROM customer";
+        $rental = $pdo->query($sql);
+
+        return $rental->fetchAll();
+    }catch(PDOException $error){
+        throw $error;
+    }
+}
