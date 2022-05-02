6b0bbb5eff227b311137a6e8ae13fb8a60f7d16b
Siiri Poropudas <n1posi00@students.oamk.fi>
2022-05-02 15:10:52 +0300

    Customer & movie names now have correct placement

--- public/rentHistory.php
+++ public/rentHistory.php
@@ -4,19 +4,9 @@ include MODULES_DIR."rentals.php";
 
 
 $rental = getRents();
-$movieName = getMovieName();
-$customerName = getCustomerName();
 
 foreach($rental as $r){
-    echo "<li>".$r["startDate"]." - ".$r["endDate"];
-}
-
-foreach($movieName as $mn){
-    echo $mn["Name"];
-}
-
-foreach($customerName as $cn){
-    echo $cn["FirstName"]." ".$cn["SurName"];
+    echo "<li>".$r["startDate"]." - ".$r["endDate"]."  ".$r["FirstName"]."  ".$r["SurName"]."  ".$r["MovieName"];
 }
 
 include TEMPLATES_DIR.'foot.php';
--- src/modules/rentals.php
+++ src/modules/rentals.php
@@ -5,38 +5,9 @@ function getRents(){
 
     try{
         $pdo = openDB();
-        $sql = "SELECT *
-        FROM rent";
-        $rental = $pdo->query($sql);
-
-        return $rental->fetchAll();
-    }catch(PDOException $error){
-        throw $error;
-    }
-}
-
-function getMovieName(){
-    require_once MODULES_DIR.'db.php';
-
-    try{
-        $pdo = openDB();
-        $sql = "SELECT Name
-        FROM movie";
-        $rental = $pdo->query($sql);
-
-        return $rental->fetchAll();
-    }catch(PDOException $error){
-        throw $error;
-    }
-}
-
-function getCustomerName(){
-    require_once MODULES_DIR.'db.php';
-
-    try{
-        $pdo = openDB();
-        $sql = "SELECT FirstName, SurName
-        FROM customer";
+        $sql = "SELECT customer.CustomerID, FirstName, SurName, movie.MovieID, MovieName, RentID, startDate, endDate
+        FROM customer, movie, rent
+        WHERE customer.CustomerID = rent.CustomerID && movie.MovieID = rent.MovieID";
         $rental = $pdo->query($sql);
 
         return $rental->fetchAll();
